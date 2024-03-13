66d5690a31635f8e2364bbf46c16574ef020e0a5
Aline Mattos <abelomo95@gmail.com>
2024-03-13 12:22:48 -0300

    exercise 14

---
+++ src/ex14/string.isPalindromo.kt
@@ -0,0 +1,25 @@
+package ex14
+
+//Create an extension function called isPalindrome for the String class that checks whether the string is a palindrome.
+// The function must ignore whitespace and be case-insensitive. Then use this function to check whether some words are palindromes.
+
+fun String.isPalindrome(): Boolean {
+    val formated = this.replace("\\s".toRegex(), "").lowercase()
+    val reversed = formated.reversed()
+    return formated == reversed
+}
+
+fun main() {
+    // Exemplos de uso da função de extensão
+    val test1 = "hello"
+    val test2 = "Ana"
+    val test3 = "Socorram me subi no onibus em Marrocos"
+    val test4 = "Roma e Amor"
+    val test5 = "tchau e bença"
+
+    println("A frase ou palavra '$test1' é palíndromo? ${test1.isPalindrome()}")
+    println("A frase ou palavra '$test2' é palíndromo? ${test2.isPalindrome()}")
+    println("A frase ou palavra '$test3' é palíndromo? ${test3.isPalindrome()}")
+    println("A frase ou palavra '$test4' é palíndromo? ${test4.isPalindrome()}")
+    println("A frase ou palavra '$test5' é palíndromo? ${test5.isPalindrome()}")
+}
