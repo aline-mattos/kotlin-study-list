d821c0f79ea718c42517a4f018ecd1f18774f3d7
Aline Mattos <abelomo95@gmail.com>
2024-03-13 10:49:15 -0300

    exercise 3

---
+++ src/ex3/alphabetical-order.kt
@@ -0,0 +1,26 @@
+package ex3
+
+class Person(val name: String, val age: Int)
+
+fun main(){
+    val people = listOf(
+        Person("Vladimir", 58),
+        Person("Aline", 28),
+        Person("Beatriz", 20),
+        Person("Amanda", 24),
+        Person("Anderson", 25),
+        Person("Daniel", 26),
+        Person("Adriana", 24),
+        Person("Lizandra", 58),
+        Person("Lara", 28),
+        Person("Murilo", 30)
+    )
+
+    val alphaOrder = people.sortedBy { it.name }
+
+    println("Nomes em Ordem Alfabética: ")
+
+    for (person in alphaOrder) {
+        println("Nome: ${person.name}, Idade: ${person.age}")
+    }
+}
